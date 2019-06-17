d4e06ff6ce9c0f3a85ee16e0277dcab6a5e3a8cb
calee14 <caplee1000@gmail.com>
2019-06-17 16:42:23 -0700

    making a service to add ingredients to the db

--- AllergyIndicator/Services/IngredientService.swift
+++ AllergyIndicator/Services/IngredientService.swift
@@ -10,3 +10,33 @@ import Foundation
 import FirebaseDatabase
 import FirebaseAuth
 
+struct IngredientService {
+    
+    private static let DatabaseIngredientsPath = Constants.DatabasePath.userIngredients
+    
+    static func setIngredient(for user: User, ingredient: Ingredient, completion: @escaping ([Ingredient]) -> Void) {
+        var results = [String : Any]()
+        
+        let time = NSDate().timeIntervalSince1970
+        results[ingredient.getIngredientName()] = time
+        
+        let ref = Database.database().reference().child(DatabaseIngredientsPath).child(user.uid)
+        
+        ref.updateChildValues(results) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                guard let dict = snapshot.value as? [String : TimeInterval] else { return }
+                let sortedDict = dict.sorted(by: { $0.value > $1.value })
+                
+                var ingredients = [Ingredient]()
+                for ingredient in sortedDict {
+                    ingredients.append(Ingredient(ingredient.key))
+                }
+                completion(ingredients)
+            })
+        }
+    }
+}
