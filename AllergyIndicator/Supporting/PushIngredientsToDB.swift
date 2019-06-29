9fecaab17603a1d32b7373ef3b2c4c9dd7052df8
calee14 <caplee1000@gmail.com>
2019-06-28 17:57:22 -0700

    adding the func to push the data to the db

--- AllergyIndicator/Supporting/PushIngredientsToDB.swift
+++ AllergyIndicator/Supporting/PushIngredientsToDB.swift
@@ -10,6 +10,9 @@ import Foundation
 import FirebaseDatabase
 
 struct PushIngredientsToDB {
+    
+    private static let DatabaseIngredientsPath = Constants.DatabasePath.userIngredients
+    
     static func retrieveLocalData() -> [String] {
         let file = "indv_ingredientv1"
         if let path = Bundle.main.path(forResource: file, ofType: "txt") {
@@ -23,7 +26,21 @@ struct PushIngredientsToDB {
         }
         return [String]()
     }
-    static func addDataToFirebase(ingredientData: [String]) {
+    static func addDataToFirebase(ingredientData: [String], success: @escaping (Bool) -> Void) {
         
+        let ref = Database.database().reference().child(DatabaseIngredientsPath)
+        
+        for ingredient in ingredientData {
+            let ingredientDict = [ingredient : true]
+            
+            ref.updateChildValues(ingredientDict) { (error, ref) in
+                if let error = error {
+                    print("we have an error while trying to add the ingredients to the db")
+                    assertionFailure(error.localizedDescription)
+                    return
+                }
+            }
+        }
+        success(true)
     }
 }
