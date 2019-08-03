700f2e3ca2efca28805a7bca045fbff508b12b46
calee14 <caplee1000@gmail.com>
2019-08-03 12:54:10 -0700

    creating the vc file for the login

---
+++ AllergyIndicator/ViewControllers/ActualUserLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ActualUserLoginViewController.swift
+//  AllergyIndicator
+//
+//  Created by Cappillen on 8/3/19.
+//  Copyright © 2019 Cappillen. All rights reserved.
+//
+
+import UIKit
+
+class ActualUserLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
