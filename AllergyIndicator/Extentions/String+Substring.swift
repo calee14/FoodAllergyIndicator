ff77b9cf85897c706dbe7a0658109fd3387a48d6
calee14 <caplee1000@gmail.com>
2019-08-04 17:39:06 -0700

    adding the extension to check if string is valid email

--- AllergyIndicator/Extentions/String+Substring.swift
+++ AllergyIndicator/Extentions/String+Substring.swift
@@ -66,6 +66,14 @@ extension Substring {
     }
 }
 
+extension String {
+    func isValidEmail() -> Bool {
+        // here, `try!` will always succeed because the pattern is valid
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
+    }
+}
+
 extension NSMutableAttributedString {
     @discardableResult func bold(_ text: String) -> NSMutableAttributedString {
         let attrs: [NSAttributedString.Key: Any] = [.font: UIFont(name: "AvenirNext-Medium", size: 12)!]
