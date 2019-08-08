4540fef066923460e6086287a0e091897d4aeaf4
calee14 <caplee1000@gmail.com>
2019-08-07 17:22:27 -0700

    making some ui effects for the logout button

--- AllergyIndicator/ViewControllers/ProfileViewController.swift
+++ AllergyIndicator/ViewControllers/ProfileViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var logoutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        setupLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupLayout() {
+        
+        let red = UIColor.init(red: 255, green: 38, blue: 0)
+        
+        // Styles for the logout button
+        logoutButton.layer.borderColor = red.cgColor
+        logoutButton.layer.borderWidth = CGFloat(2)
+        logoutButton.layer.cornerRadius = CGFloat(10)
+        logoutButton.backgroundColor = .clear
+        logoutButton.clipsToBounds = true
+        self.logoutButton.setBackgroundColor(color: .white, forState: .normal)
     }
-    */
-
+    
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        let red = UIColor.init(red: 255, green: 38, blue: 0)
+        self.logoutButton.setTitleColor(red, for: .normal)
+    }
+    @IBAction func logoutButtonHighlight(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.1) {
+            let red = UIColor.init(red: 255, green: 38, blue: 0)
+            self.logoutButton.backgroundColor = red
+            self.logoutButton.setTitleColor(.white, for: .normal)
+        }
+        
+    }
+    
 }
